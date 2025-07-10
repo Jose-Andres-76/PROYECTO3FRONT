21992e7917143be5d8efa927fc6069dbd4251a58
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-07-09 19:43:44 -0600

    Update
    Update Landing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/app-layout/elements/navbar/navbar.component';
-import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
-import { InfoCardsComponent } from '../../components/info-cards/info-cards.component';
+import { HeroSectionComponent } from '../../components/landingComponents/hero-section/hero-section.component';
+import { InfoCardsComponent } from '../../components/landingComponents/info-cards/info-cards.component';
 import { FooterComponent } from '../../components/app-layout/elements/footer/footer.component';
 
 @Component({
