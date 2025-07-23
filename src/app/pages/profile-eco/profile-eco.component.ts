fbf288ed556516206addcbe89d0b67b504b9863c
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-07-22 22:03:26 -0600

    ProfileEco Create it
    ProfileEco Create it

---
+++ src/app/pages/profile-eco/profile-eco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-eco',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-eco.component.html',
+  styleUrl: './profile-eco.component.scss'
+})
+export class ProfileEcoComponent {
+
+}
